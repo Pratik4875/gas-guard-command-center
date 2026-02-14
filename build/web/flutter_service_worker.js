ca237fd21664ae1e00e005d25f51b3c778a92c75
Pratik kadam <100049917+Pratik4875@users.noreply.github.com>
2026-02-14 10:41:39 +0530

    Add script to force unregister service workers

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "00b634e32a9cb69e79f49bad6702e535",
+const RESOURCES = {"flutter_bootstrap.js": "c35723495ee9cae9bd25aa984c0d5586",
 "version.json": "3643916c0b4b30d8268bf93f7156ac97",
-"index.html": "83f57030e1aca7bce4dd92b1a47aacb9",
-"/": "83f57030e1aca7bce4dd92b1a47aacb9",
+"index.html": "4fb559f769000fd0084ebfa43ef30cd6",
+"/": "4fb559f769000fd0084ebfa43ef30cd6",
 "main.dart.js": "6bab4229d39b8d991d961cdee3b833a4",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
